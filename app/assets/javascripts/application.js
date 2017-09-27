7623ce2ffc1795b8ae632d4c99a3cc867eed8041
Wendelcorp <bryce@brycewendelaar.com>
2017-09-26 23:36:46 -0400

    Implemented first stage of calculator

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,58 +29,154 @@ $(function() {
 
     $("#booking-form").formToWizard();
 
-    var numberOfGuests = 0;
-    var pricePerPerson = 0;
-    type = $("#booking_bar_type").val();
-    numberOfGuests = $("#booking_guests").val();
+    // var numberOfGuests = 0;
+    // var pricePerPerson = 0;
+    // type = $("#booking_bar_type").val();
+    // numberOfGuests = $("#booking_guests").val();
 
-    var calculateTotal = function() {
-      if (type === "One Feature") {
-        if (numberOfGuests < 50) {
+
+    // var calculateTotal = function() {
+    //   if (type === "One Feature") {
+    //     if (numberOfGuests < 50) {
+    //       pricePerPerson = 20
+    //     }
+    //     else if ((numberOfGuests > 50) && (numberOfGuests <= 199)) {
+    //       pricePerPerson = 15
+    //     }
+    //     else if (numberOfGuests > 199) {
+    //       pricePerPerson = 10
+    //     }
+    //   }
+    //   else if (type === "Two Features") {
+    //     if (numberOfGuests < 50) {
+    //       pricePerPerson = 25
+    //     }
+    //     else if ((numberOfGuests > 50) && (numberOfGuests <= 199)) {
+    //       pricePerPerson = 18
+    //     }
+    //     else if (numberOfGuests > 199) {
+    //       pricePerPerson = 13
+    //     }
+    //   }
+    //   else if (type === "Three Features") {
+    //     if (numberOfGuests < 50) {
+    //       pricePerPerson = 30
+    //     }
+    //     else if ((numberOfGuests > 50) && (numberOfGuests <= 199)) {
+    //       pricePerPerson = 20
+    //     }
+    //     else if (numberOfGuests > 199) {
+    //       pricePerPerson = 15
+    //     }
+    //   }
+    //   total = numberOfGuests * pricePerPerson
+    //   $('#current-price').html(total)
+    // }
+    // $("#booking_bar_type").change(function() {
+    //   type = $("#booking_bar_type").val();
+    //   numberOfGuests = $("#booking_guests").val();
+    //   calculateTotal()
+    // });
+    // $("#booking_guests").change(function() {
+    //   type = $("#booking_bar_type").val();
+    //   numberOfGuests = $("#booking_guests").val();
+    //   calculateTotal()
+    // });
+
+    function calculateNumberOfServices() {
+      var numberOfServices = 0
+      if (($('.select-beer').val() !== '') && ($('.select-beer').val() !== 'No beer please')) {
+        numberOfServices += 1
+      }
+      if (($('.select-wine').val() !== '') && ($('.select-wine').val() !== 'No wine please')) {
+        numberOfServices += 1
+      }
+      if (($('.select-spirits').val() !== '') && ($('.select-spirits').val() !== 'No spirits please')) {
+        numberOfServices += 1
+      }
+      return numberOfServices
+    }
+
+    function calculateAddOns() {
+      var addOnPrice = 0
+      if ($('.select-beer').val() == 'Special Request (+$5/person)') {
+        addOnPrice += 5
+      }
+      if ($('.select-wine').val() == 'Special Request (+$5/person)') {
+        addOnPrice += 5
+      }
+      if ($('.select-spirits').val() == 'Special Request (+$5/person)') {
+        addOnPrice += 5
+      }
+      return addOnPrice
+    }
+
+    function calculatePricePerPerson() {
+      var numberOfGuests = $("#guests").val();
+      var pricePerPerson
+      var numberOfServices = calculateNumberOfServices()
+
+      if (numberOfServices == 1) {
+
+        if (numberOfGuests < 19) {
+          pricePerPerson = 30
+        } else if (numberOfGuests >= 20 && numberOfGuests < 50) {
           pricePerPerson = 20
-        }
-        else if ((numberOfGuests > 50) && (numberOfGuests <= 199)) {
+        } else if (numberOfGuests >= 50 && numberOfGuests < 100) {
           pricePerPerson = 15
+        } else if (numberOfGuests >= 100 && numberOfGuests < 150) {
+          pricePerPerson = 13
+        } else if (numberOfGuests >= 150 && numberOfGuests < 300) {
+          pricePerPerson = 10
+        } else if (numberOfGuests >= 300 && numberOfGuests < 500) {
+          pricePerPerson = 8
         }
-        else if (numberOfGuests > 199) {
+
+      } else if (numberOfServices == 2) {
+
+        if (numberOfGuests < 19) {
+          pricePerPerson = 35
+        } else if (numberOfGuests >= 20 && numberOfGuests < 50) {
+          pricePerPerson = 25
+        } else if (numberOfGuests >= 50 && numberOfGuests < 100) {
+          pricePerPerson = 18
+        } else if (numberOfGuests >= 100 && numberOfGuests < 150) {
+          pricePerPerson = 15
+        } else if (numberOfGuests >= 150 && numberOfGuests < 300) {
+          pricePerPerson = 13
+        } else if (numberOfGuests >= 300 && numberOfGuests < 500) {
           pricePerPerson = 10
         }
-      }
-      else if (type === "Two Features") {
-        if (numberOfGuests < 50) {
-          pricePerPerson = 25
-        }
-        else if ((numberOfGuests > 50) && (numberOfGuests <= 199)) {
-          pricePerPerson = 18
-        }
-        else if (numberOfGuests > 199) {
-          pricePerPerson = 13
-        }
-      }
-      else if (type === "Three Features") {
-        if (numberOfGuests < 50) {
-          pricePerPerson = 30
-        }
-        else if ((numberOfGuests > 50) && (numberOfGuests <= 199)) {
-          pricePerPerson = 20
-        }
-        else if (numberOfGuests > 199) {
-          pricePerPerson = 15
-        }
-      }
-      total = numberOfGuests * pricePerPerson
-      $('#current-price').html(total)
-    }
-    $("#booking_bar_type").change(function() {
-      type = $("#booking_bar_type").val();
-      numberOfGuests = $("#booking_guests").val();
-      calculateTotal()
-    });
-    $("#booking_guests").change(function() {
-      type = $("#booking_bar_type").val();
-      numberOfGuests = $("#booking_guests").val();
-      calculateTotal()
-    });
 
-    calculateTotal()
+      } else if (numberOfServices == 3) {
+
+        if (numberOfGuests < 19) {
+          pricePerPerson = 40
+        } else if (numberOfGuests >= 20 && numberOfGuests < 50) {
+          pricePerPerson = 30
+        } else if (numberOfGuests >= 50 && numberOfGuests < 100) {
+          pricePerPerson = 20
+        } else if (numberOfGuests >= 100 && numberOfGuests < 150) {
+          pricePerPerson = 18
+        } else if (numberOfGuests >= 150 && numberOfGuests < 300) {
+          pricePerPerson = 15
+        } else if (numberOfGuests >= 300 && numberOfGuests < 500) {
+          pricePerPerson = 12
+        }
+
+      }
+      return pricePerPerson + calculateAddOns()
+    }
+
+    function calculateTotal() {
+
+      var numberOfGuests = $("#guests").val();
+    }
+
+    $('form :input').change(function() {
+      calculateTotal()
+      console.log(calculateNumberOfServices())
+      console.log("price per person " + calculatePricePerPerson())
+      $('#current-price').html(calculatePricePerPerson())
+    });
 });
